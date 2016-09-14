f153d46d146d033e7167c444e52e972b9c65a13b
marjamis <marjamis@amazon.com>
2016-09-14 11:39:11 +0000

    A few updates testing a few things though it all works fine.

--- application/src/cfn-signaler/main.go
+++ application/src/cfn-signaler/main.go
@@ -2,51 +2,54 @@ package main
 
 import (
 	"fmt"
-	log "github.com/Sirupsen/logrus"
+	"os"
+	"errors"
+	"html/template"
+	"io/ioutil"
+	"net/http"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
-	"html/template"
-	"io/ioutil"
-	"net/http"
-	"os"
+	log "github.com/Sirupsen/logrus"
 )
+
 
 type Response struct {
 	Signal string
+	Error error
 }
 
-func signal(send string) {
+func signal(send string) (err error){
 	session := session.New()
 	metadata := ec2metadata.New(session)
 
 	if !metadata.Available() {
 		log.Info("Error: Metadata not available.")
-		return
+		return errors.New("Error: Metadata not available.")
 	}
 
 	region, err := metadata.Region()
 	if err != nil {
 		log.Error(err)
-		return
+		return err
 	}
 
 	instance_id, err := metadata.GetMetadata("instance-id")
 	if err != nil {
 		log.Error(err)
-		return
+		return err
 	}
 
-	lid := os.Getenv("LOGICALID")
-	sn := os.Getenv("STACKNAME")
+	logical_id := os.Getenv("LOGICALID")
+	stack_name := os.Getenv("STACKNAME")
 
 	cfn_config := aws.NewConfig().WithRegion(region)
 	svc := cloudformation.New(session, cfn_config)
 
 	params := &cloudformation.SignalResourceInput{
-		LogicalResourceId: aws.String(lid),
-		StackName:         aws.String(sn),
+		LogicalResourceId: aws.String(logical_id),
+		StackName:         aws.String(stack_name),
 		Status:            aws.String(send),
 		UniqueId:          aws.String(instance_id),
 	}
@@ -54,10 +57,11 @@ func signal(send string) {
 	resp, err := svc.SignalResource(params)
 	if err != nil {
 		log.Error(err)
-		return
+		return err
 	}
 
-	log.Info(resp)
+	log.Info("signal ", resp)
+	return nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -83,27 +87,30 @@ func signalHandler(w http.ResponseWriter, r *http.Request) {
 	value := r.FormValue("send")
 
 	var text string
+	var err error
 	if value == "SUCCESS" {
-		signal("SUCCESS")
-		text = "Success signal sent"
+		err = signal("SUCCESS")
+		text = "Success signal"
 	} else if value == "FAILURE" {
-		signal("FAILURE")
-		text = "Failed signal sent"
+		err = signal("FAILURE")
+		text = "Failed signal"
 	} else {
+		err = errors.New("Invalid signal type specified in POST")
 		text = "Invalid signal type"
 	}
 
 	t, _ := template.ParseFiles("templates/signal.html")
-	data := &Response{Signal: text}
+	data := &Response{Signal: text, Error: err}
 	t.Execute(w, data)
 	log.Info("signalHandler IP: ", r.RemoteAddr, " signal: ", value)
 }
 
 func main() {
+        port := os.Getenv("PORT")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/signal/", signalHandler)
-	log.Info("Listening on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
+	log.Info("Listening on port " + port + "...")
+	err := http.ListenAndServe(":" + port, nil)
 	if err != nil {
 		log.Error(err)
 	}
